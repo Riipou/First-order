98b5e9734b9671f016ff5ec56304c11e96643618
riipou <joakim.lefebvre23@gmail.com>
2024-03-05 11:26:19 +0100

    prepocessing

--- functions.py
+++ functions.py
@@ -1,2 +1,33 @@
-def least_square(a,b):
+import numpy as np
+
+
+def one_hot_encoding(y):
+    new_y = []
+    for label in y:
+        label_vector = np.zeros(20)
+        label_vector[label - 1] = 1
+        new_y.append(label_vector)
+    y = np.array(new_y)
+    return y
+
+
+def shuffle(X, y):
+    index = np.arange(np.shape(X)[1])
+    np.random.shuffle(index)
+    X = X[:, index]
+    y = y.T[:, index]
+    return X, y
+
+
+def split_data(X, y, split_value=0.8):
+    m = X.shape[1]
+    m = int(m * split_value)
+    X_train = X[:, :m]
+    y_train = y[:m, :]
+    X_test = X[:, m:]
+    y_test = y[m:, :]
+    return X_train, y_train, X_test, y_test
+
+
+def least_square(a, b):
     return 0
--- project.py
+++ project.py
@@ -1,62 +1,34 @@
+from functions import *
 from scipy.io import loadmat
+import numpy as np
 from sklearn.model_selection import train_test_split
 import scipy.io
 import pandas as pd
 from scipy.sparse import csr_matrix
 
-# Charger le fichier .mat
-nom_fichier_mat = 'data_doc.mat'
-donnees = loadmat(nom_fichier_mat)
+if __name__ == "__main__":
 
-# Afficher les clés (noms des variables) présentes dans le fichier .mat
-# print("Noms des variables dans le fichier :", donnees.keys())
+    # Charger le fichier .mat
+    nom_fichier_mat = 'data_doc.mat'
+    donnees = loadmat(nom_fichier_mat)
 
-# Xts combien de fois le mot dans le texte
-if 'Xts' in donnees:
-    Xts = donnees['Xts']
-    # print("Variable Xts lue avec succès :\n", Xts)
-else:
-    print("La variable 'Xts' n'a pas été trouvée dans le fichier.")
-# id texte avec classe
-if 'yts' in donnees:
-    yts = donnees['yts']
-    # print("Variable yts lue avec succès :\n", yts)
-else:
-    print("La variable 'yts' n'a pas été trouvée dans le fichier.")
-# Xvr combien de fois le mot dans le texte
-if 'Xvr' in donnees:
-    Xvr = donnees['Xvr']
-    # print("Variable Xvr lue avec succès :\n", Xvr)
-else:
-    print("La variable 'Xvr' n'a pas été trouvée dans le fichier.")
+    # Xts combien de fois le mot dans le texte
+    if 'Xts' in donnees:
+        Xts = donnees['Xts']
+    else:
+        print("La variable 'Xts' n'a pas été trouvée dans le fichier.")
 
-# Extraire les caractéristiques et les étiquettes des données chargées
-X = donnees['Xts'].T
-y = donnees['yts']
+    # id texte avec classe
+    if 'yts' in donnees:
+        yts = donnees['yts']
+    else:
+        print("La variable 'yts' n'a pas été trouvée dans le fichier.")
 
-# Créer un DataFrame avec les données
-df = pd.DataFrame(X.toarray(), columns=[f'Word_{i+1}' for i in range(X.shape[1])])
-df['Label'] = y
-
-print(df)
-# Afficher le DataFrame
-#print(df)
-"""X_train, X_test, y_train, y_test = train_test_split(Xts, yts, test_size=0.2, random_state=42)
-
-# Define the perceptron model
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(1, input_dim=X_train.shape[1], activation='sigmoid')
-])
-
-# Compile the model
-model.compile(optimizer='adam',
-              loss='MeanSquaredError',
-              metrics=['accuracy'])
-
-# Train the model
-model.fit(X_train, y_train, epochs=10, batch_size=1)
-
-# Evaluate the model
-loss, accuracy = model.evaluate(X_test, y_test)
-
-print("Accuracy:", accuracy)"""
+    # Xvr combien de fois le mot dans le texte
+    if 'Xvr' in donnees:
+        Xvr = donnees['Xvr']
+    else:
+        print("La variable 'Xvr' n'a pas été trouvée dans le fichier.")
+    yts = one_hot_encoding(yts)
+    Xts, yts = shuffle(Xts, yts)
+    X_train, y_train, X_test, y_test = split_data(Xts, yts)
